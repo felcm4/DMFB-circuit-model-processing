f8e1282b39d0a390353e7d002b1b37415442d6c8
Fred L <felcm4@mst.edu>
2019-03-28 22:00:28 -0700

    Create dmfb-hdf5-process.py

---
+++ dmfb-hdf5-process.py
@@ -0,0 +1,12 @@
+"""
+Purpose: To process HDF5 file produce by Simscape Electrical model
+         during simulation.
+
+@author: Fred Love
+"""
+
+#Imports
+import h5py
+import numpy as np
+import pandas as pd
+
