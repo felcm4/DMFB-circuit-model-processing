5aefbcaa491f61431eee701554e5a4de6c1ea10e
Fred L <felcm4@mst.edu>
2019-03-28 22:27:19 -0700

    Update dmfb-hdf5-process.py

--- dmfb-hdf5-process.py
+++ dmfb-hdf5-process.py
@@ -9,4 +9,22 @@ Purpose: To process HDF5 file produce by Simscape Electrical model
 import h5py
 import numpy as np
 import pandas as pd
+import os
+
+def load_h5data( dataset ):
+    """Load data from a given dataset
+
+    Parameters
+    ----------
+    dataset : str
+       Searches from dataset.h5 in this file's directory
+
+    Returns
+    -------
+    DataFrame
+       
+    """
+
+    file_path = Path( os.path.dirname)
+         
 
